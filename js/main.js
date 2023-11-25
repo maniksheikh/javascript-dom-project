8ab8bfc47eeccd966d3f62b256935ba3d602ac37
maniksheikh <mdmanik081498@gmail.com>
2023-11-25 18:23:01 +0600

    created the javascript  dom project

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const milestonesData = JSON.parse(data).data;
+
+function loadMilestones() {
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `<div class="milestone border-b">
+    <div class="flex">
+      <div class="checkbox"><input type="checkbox" /></div>
+      <div>
+        <p>
+          Milestone 1 name
+          <span><i class="fas fa-chevron-down"></i></span>
+        </p>
+      </div>
+    </div>
+    <div class="hidden_panel">
+      <div class="module border-b">
+        <p>Module Name</p>
+      </div>
+    </div>
+  </div> `
+};
